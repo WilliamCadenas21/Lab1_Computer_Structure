80609659e164f5d452383b80082cb49bbb69dd56
William Cadenas <williamcadenas21@gmail.com>
2018-08-09 21:19:22 -0500

    [feat]:added new logic

--- Lab1/Lab1.vhd
+++ Lab1/Lab1.vhd
@@ -17,16 +17,15 @@ use ieee.std_logic_1164.all;
 
 entity lab1 is
 port(	
-	a:		in std_logic;
+	enable:		in std_logic;
+	forward:		in std_logic;
 	clock:		in std_logic;
 	reset:		in std_logic;
-	l0:		out std_logic;
-	l1:		out std_logic;
-	l2:		out std_logic;
-	l3:		out std_logic;
-	l4:		out std_logic;
-	l5:		out std_logic;
-	l6:		out std_logic
+	display_right : out  STD_LOGIC_VECTOR (6 downto 0);
+	display_left1 : out  STD_LOGIC_VECTOR (6 downto 0);
+	
+	number: in std_logic_vector(7 to 0)
+	--number <= "00000000"
 );
 end lab1;
 
@@ -34,269 +33,89 @@ end lab1;
 
 architecture FSM of lab1 is
 
-    -- define the states of FSM model
-
-    type state_type is (S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14, S15);
-    signal next_state, current_state: state_type;
-
 begin
-    
-    -- cocurrent process#1: state registers
-    state_reg: process(clock, reset)
-    begin
-
+   -- this process verify the number of the count
+main: process(clock, reset) begin
 	if (reset='1') then
-            current_state <= S0;
-	elsif (clock'event and clock='1') then
-	    current_state <= next_state;
+		number<="00000000";
+	elsif (number = "11111111") then
+	
+	elsif(clock'event and clock='1')then
+	    
 	end if;
 
     end process;						  
 
-    -- cocurrent process#2: combinational logic
-    comb_logic: process(current_state, a)
-    begin
-
-	-- use case statement to show the 
-	-- state transistion
-
-	case current_state is
-
-	    when S0 =>	
-				l0 	<= '0';
-				l1		<= '0';
-				l2		<= '0';
-				l3		<= '0';
-				l4		<= '0';
-				l5		<= '0';
-				l6		<= '1';
-			if a='0' then
-			    next_state <= S0;
-			elsif a ='1' then
-			    next_state <= S1;
+second: process(number) begin
+	
+		one: in std_logic_vector(7 to 0);
+		--one <="0000001";
+		r1: in std_logic_vector(3 to 0);
+		l1: in std_logic_vector(3 to 0);
+		
+		
+		--Logic of the problem
+		------------------------------------------------
+			if enable='1' then
+				if forward='1' then
+					 -- Move forward
+					 number <= number + one;
+				elsif forward='0' then
+					 -- Move backward
+					 number <= number - one;
+					 
+				end if;
+				
 			end if;
 
-	    when S1 =>					
-				l0		<= '1';
-				l1		<= '0';
-				l2		<= '0';
-				l3		<= '1';
-				l4		<= '1';
-				l5		<= '1';
-				l6		<= '1';
-			if a='0' then 
-			    next_state <= S1;
-			elsif a='1' then 
-			    next_state <= S2;
-			end if;
-
-	    when S2 =>	
-				l0		<= '0';
-				l1		<= '0';
-				l2		<= '1';
-				l3		<= '0';
-				l4		<= '0';
-				l5		<= '1';
-				l6		<= '0';
-			if a='0' then
-			    next_state <= S2;
-			elsif a='1' then
-			    next_state <= S3;
-			end if;
-
-	    when S3 =>					
-				l0		<= '0';
-				l1		<= '0';
-				l2		<= '0';
-				l3		<= '0';
-				l4		<= '1';
-				l5		<= '1';
-				l6		<= '0';
-			if a='0' then 
-			    next_state <= S3;
-			elsif a='1' then 
-			    next_state <= S4;
-			end if;
-			
-		when S4 =>					
-				l0		<= '1';
-				l1		<= '0';
-				l2		<= '0';
-				l3		<= '1';
-				l4		<= '1';
-				l5		<= '0';
-				l6		<= '0';
-			if a='0' then 
-			    next_state <= S4;
-			elsif a='1' then 
-			    next_state <= S5;
-			end if;
-			
-		when S5 =>					
-				l0		<= '0';
-				l1		<= '1';
-				l2		<= '0';
-				l3		<= '0';
-				l4		<= '1';
-				l5		<= '0';
-				l6		<= '0';
-			if a='0' then 
-			    next_state <= S5;
-			elsif a='1' then 
-			    next_state <= S6;
-			end if;
-			
-		when S6 =>					
-				l0		<= '0';
-				l1		<= '1';
-				l2		<= '0';
-				l3		<= '0';
-				l4		<= '0';
-				l5		<= '0';
-				l6		<= '0';
-			if a='0' then 
-			    next_state <= S6;
-			elsif a='1' then 
-			    next_state <= S7;
-			end if;
-			
-		when S7 =>					
-				l0		<= '0';
-				l1		<= '0';
-				l2		<= '0';
-				l3		<= '1';
-				l4		<= '1';
-				l5		<= '1';
-				l6		<= '1';
-			if a='0' then 
-			    next_state <= S7;
-			elsif a='1' then 
-			    next_state <= S8;
-			end if;
-			
-		when S8 =>					
-				l0		<= '0';
-				l1		<= '0';
-				l2		<= '0';
-				l3		<= '0';
-				l4		<= '0';
-				l5		<= '0';
-				l6		<= '0';
-			if a='0' then 
-			    next_state <= S8;
-			elsif a='1' then 
-			    next_state <= S9;
-			end if;
-			
-		when S9 =>					
-				l0		<= '0';
-				l1		<= '0';
-				l2		<= '0';
-				l3		<= '1';
-				l4		<= '1';
-				l5		<= '0';
-				l6		<= '0';
-			if a='0' then 
-			    next_state <= S9;
-			elsif a='1' then 
-			    next_state <= S10;
-			end if;
-			
-			when S10 =>					
-				l0		<= '0';
-				l1		<= '0';
-				l2		<= '0';
-				l3		<= '1';
-				l4		<= '0';
-				l5		<= '0';
-				l6		<= '0';
-			if a='0' then 
-			    next_state <= S10;
-			elsif a='1' then 
-			    next_state <= S11;
-			end if;
-			
-			when S11 =>					
-				l0		<= '1';
-				l1		<= '0';
-				l2		<= '0';
-				l3		<= '0';
-				l4		<= '0';
-				l5		<= '1';
-				l6		<= '0';
-			if a='0' then 
-			    next_state <= s11;
-			elsif a='1' then 
-			    next_state <= S12;
-			end if;
-			
-			when S12 =>					
-				l0		<= '0';
-				l1		<= '1';
-				l2		<= '1';
-				l3		<= '0';
-				l4		<= '0';
-				l5		<= '0';
-				l6		<= '1';
-			if a='0' then 
-			    next_state <= S12;
-			elsif a='1' then 
-			    next_state <= S13;
-			end if;
-
-			when S13 =>					
-				l0		<= '1';
-				l1		<= '0';
-				l2		<= '0';
-				l3		<= '0';
-				l4		<= '0';
-				l5		<= '1';
-				l6		<= '0';
-			if a='0' then 
-			    next_state <= S13;
-			elsif a='1' then 
-			    next_state <= S14;
-			end if;
-
-			when S14 =>					
-				l0		<= '0';
-				l1		<= '1';
-				l2		<= '1';
-				l3		<= '0';
-				l4		<= '0';
-				l5		<= '0';
-				l6		<= '0';
-			if a='0' then 
-			    next_state <= S14;
-			elsif a='1' then 
-			    next_state <= S15;
-			end if;
-
-			when S15 =>					
-				l0		<= '0';
-				l1		<= '1';
-				l2		<= '1';
-				l3		<= '1';
-				l4		<= '0';
-				l5		<= '0';
-				l6		<= '0';
-			if a='0' then 
-			    next_state <= S15;
-			elsif a='1' then 
-			    next_state <= S0;
-			end if;
-
-	    when others =>
-				l0		<= '1';
-				l1		<= '1';
-				l2		<= '0';
-				l3		<= '1';
-				l4		<= '1';
-				l5		<= '0';
-				l6		<= '1';
-			next_state <= S0;
-
-	end case;
+			--Divide vector called number
+		------------------------------------------------	
+			r1(0) <= number(4);
+			r1(1) <= number(5);
+			r1(2) <= number(6);
+			r1(3) <= number(7);
+			--------------------------------------
+			l1(0) <= number(4);
+			l1(1) <= number(5);
+			l1(2) <= number(6);
+			l1(3) <= number(7);
+		------------------------------------------------
+		
+		--Call fuction
+		------------------------------------------------
+		display_right <= choose_output(r1);
+		display_left <= choose_output(l1);
+		------------------------------------------------
+					 
+		--Function
+		------------------------------------------------
+		  function choose_output (
+			 number : in std_logic_vector(3 downto 0))
+			 return std_logic_vector is
+			 variable vector : std_logic_vector(6 downto 0);
+		  begin
+			 with number select
+				 vector <="0000001" when "0000",--0
+							"1001111"  when "0001",--1
+							"0010010" when "0010",--2
+							"0000110" when "0011",--3
+							"1001100" when "0100",--4
+							"0100100"  when "0101",--5
+							"0100000" when "0110",--6
+							"0001111"when "0111",--7
+							"0000000" when "1000",--8
+							"0001100" when "1001",--9
+							"0001000"when "1010",--A
+							"1100000" when "1011",--b
+							"0110001" when "1100",--C
+							"1000010" when "1101",--d
+							"0110000"  when "1110",--e
+							"0111000" when "1111",--f
+							"0000000" when others;
+			 return std_logic_vector(vector);
+			 
+		  end;
+		-------------------------------------------------  
 
     end process;
 
